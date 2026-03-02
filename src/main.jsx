87e4ace5126aa5402d12bd5fc25160e3c7a67939
aaakum4 <necroticsanjiv@gmail.com>
2026-03-03 10:15:04 +1100

    added tracking and analystics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { TimeProvider } from "./core/TimeProvider.jsx";
 import { AppProvider } from "./core/AppContext.jsx";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import "../styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -10,6 +12,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <TimeProvider>
       <AppProvider>
         <App />
+        <Analytics />
+        <SpeedInsights />
       </AppProvider>
     </TimeProvider>
   </React.StrictMode>
